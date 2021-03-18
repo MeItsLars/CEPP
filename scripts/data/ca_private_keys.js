f8c1b74e82d8d9ba8879c4b49efcba10b41f2592
MeItsLars <larsjeurissen@hotmail.nl>
2021-03-18 17:27:21 +0100

    Changed key placement in data files

--- scripts/data/ca_private_keys.js
+++ scripts/data/ca_private_keys.js
@@ -3,7 +3,8 @@ caPrivateKeys = {
         'algorithm': 'ecdsa',
         'parameters': {
             'curve': 'secp256k1',
-            'private-key': '4f77a087f11c319df6842928e92c1a3941d4d94386a0209a777d81bca6461f6d'
+            'private-key': '4f77a087f11c319df6842928e92c1a39' +
+                '41d4d94386a0209a777d81bca6461f6d'
         }
     }
 }
--- scripts/data/trusted_ca_public_keys.js
+++ scripts/data/trusted_ca_public_keys.js
@@ -4,7 +4,10 @@ const trusted_ca_public_keys = {
         'algorithm': 'ecdsa',
         'parameters': {
             'curve': 'secp256k1',
-            'public-key': '049a55a04ad8538e460dc175bb027859d32eb88208b8ecb5ac2d16afaf19079af008db4d349cd1098bc758796c40b4fb2b75da3557d4887c77ece7af759f2a7143'
+            'public-key': '049a55a04ad8538e460dc175bb02785' +
+                '9d32eb88208b8ecb5ac2d16afaf1907' +
+                '9af008db4d349cd1098bc758796c40b' +
+                '4fb2b75da3557d4887c77ece7af759f2a7143'
         }
     }
 }
